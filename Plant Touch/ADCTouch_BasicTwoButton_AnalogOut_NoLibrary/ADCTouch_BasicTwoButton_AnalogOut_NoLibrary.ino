c2228731b467f0c143e180857882935ffd52d613
Andrew Quitmeyer <andrew.quitmeyer@gmail.com>
2020-07-31 10:27:12 -0500

    Update ADCTouch_BasicTwoButton_AnalogOut_NoLibrary.ino

--- Plant Touch/ADCTouch_BasicTwoButton_AnalogOut_NoLibrary/ADCTouch_BasicTwoButton_AnalogOut_NoLibrary.ino
+++ Plant Touch/ADCTouch_BasicTwoButton_AnalogOut_NoLibrary/ADCTouch_BasicTwoButton_AnalogOut_NoLibrary.ino
@@ -29,6 +29,9 @@ void setup()
 
     ref0 = ADCTouch.read(A0, 500);    //create reference values to 
     ref1 = ADCTouch.read(A1, 500);    //account for the capacitance of the pad
+
+    pinMode(12,OUTPUT);
+    digitalWrite(12,HIGH);
 } 
 
 void loop() 
@@ -39,25 +42,25 @@ void loop()
     value0 -= ref0;       //remove offset
     value1 -= ref1;
 
-    Serial.print(value0 > threshold);    //send (boolean) pressed or not pressed
-    Serial.print("\t");           //use if(value > threshold) to get the state of a button
+    //Serial.print(value0 > threshold);    //send (boolean) pressed or not pressed
+   // Serial.print("\t");           //use if(value > threshold) to get the state of a button
 
-    Serial.print(value1 > threshold);
-    Serial.print("\t\t");
+   // Serial.print(value1 > threshold);
+   // Serial.print("\t\t");
 
-    Serial.print(value0);             //send actual reading
-    Serial.print("\t");
+    Serial.println(value0);             //send actual reading
+    //Serial.print("\t");
   
-    Serial.print(value1);
-    delay(50);
+    //Serial.println(value1);
+    delay(5);
 
 val0constrained = map(value0,val0low,val0high,0,255); // map from 0 to 255 for output for an LED or Buzzer
    
 val0constrained = constrain(val0constrained,0,255); // extra make sure it doesn't go past
 
-    Serial.print("\t val0constrained = ");
+//    Serial.print("\t val0constrained = ");
   
-    Serial.println(val0constrained);
+  //  Serial.println(val0constrained);
 
     analogWrite(analogPinOut, val0constrained);
 
